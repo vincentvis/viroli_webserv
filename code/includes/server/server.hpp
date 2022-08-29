afb5372022ba7ca04116ee60c749b0a07538019d
lisa_codam <lisa.vlamings@gmail.com>
2022-08-29 15:01:58 +0200

    setup Clion + empty class

---
+++ code/includes/server/server.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Lisa Vlamings on 8/29/22.
+//
+
+#ifndef WEBSERVER_SERVER_HPP
+#define WEBSERVER_SERVER_HPP
+
+
+class server {
+
+};
+
+
+#endif //WEBSERVER_SERVER_HPP
---
+++ code/source_files/server/server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lisa Vlamings on 8/29/22.
+//
+
+#include "../../includes/server/server.hpp"
