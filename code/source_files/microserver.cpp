110b1dd7ac5afdc0b8c4e3f1b2fa33b952b23718
Ronald Heuts <rheuts@f1r2s15.codam.nl>
2022-09-01 17:38:04 +0200

    poll/multiplexing
    First shot at multiplexing.
    
    Can handle multiple connections;
    Will poll the connections.
    
    side note: MAX_CONN doesn't seem to affect listen()

--- code/source_files/microserver.cpp
+++ code/source_files/microserver.cpp
@@ -1,14 +1,6 @@
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-
-#include <iostream>
 
 #define PORT 8080
-#define MAX_CONN 10
+#define MAX_CONN 1 // does nothing?
 
 // struct addrinfo {
 //   int              ai_flags;
@@ -21,12 +13,22 @@
 //   struct addrinfo *ai_next;
 // };
 
+// struct sockaddr_in {
+//   sa_family_t      sin_family;     /* address family: AF_INET */
+//   in_port_t        sin_port;       /* port in network byte order */
+//   struct in_addr   sin_addr;       /* internet address */
+// };
 
+// /* Internet address */
+// struct in_addr {
+//   uint32_t         s_addr;         /* address in network byte order */
+// };
 
 /*
 ** NETCAT USAGE
 ** Client mode (connect to somewhere):
 ** nc [options] [IP address/host name] [port]
+** nc localhost [port]
 */
 
 /*
@@ -34,72 +36,182 @@
 ** lsof -i -P | grep LISTEN | grep :$PORT
 */
 
-/* 
-** 1. establish a connection and receive and send a set amount of data
-** 2. receive and send any amount of data
-** 3. establish multiple connections in a queue
-** 4. have non-blocking connections
+/*
+** Poll()
+** Store socket descriptors in a poll struct, together with on what events to
+** act (POLLIN/POLLOUT)
+** Keep track of listening sockets and new (accepted) sockets in an array;
+** When POLLIN occurs in revent, check socket is a listening socket or a client
+** socket;
 */
 
-int main (void) {
-  char buffer[128] = {0};
-  int sd = 0;
-  int new_sd = 0;
+/*
+** 1. [X] establish a connection and receive and send a set amount of data
+** 2. [ ] receive and send any amount of data
+** 3. [ ] establish multiple connections (in a queue)
+** 4. [ ] have non-blocking connections
+*/
 
-  /* useful information on structures used: https://beej.us/guide/bgnet/html/#structs */
+#include "microserver.hpp"
 
+uServer::uServer(std::vector<uint16_t> &ports) : _ports(ports){};
+
+uServer::uServer(void){};
+
+uServer::~uServer(void){};
+
+/*
+  Iterate ports from config
+  Store ports in "sockaddr_in" struct
+  Store "sockaddr_in" structs in vector
+  Create sockets
+  Bind sockets to ports
+  Add sockets to vector
+*/
+
+void uServer::init(void) {
+  int fd = 0;
+  int opt = 1;
   struct sockaddr_in server;
-  struct sockaddr_in client; // used/filled in accept() call
+  socklen_t addrlen = sizeof(server);
+  struct pollfd pfd;
 
-  memset(&server, 0, sizeof(server));
-  server.sin_family = AF_INET;
-  server.sin_port = htons(PORT); // port number will be pulled from config
-  server.sin_addr.s_addr = INADDR_ANY;
+  for (std::vector<uint16_t>::iterator it = _ports.begin(); it != _ports.end();
+       ++it) {
 
-  /* 
-  ** If the protocol parameter is set to 0, the system selects the default protocol number for the domain and socket type requested.
-  ** https://www.ibm.com/docs/en/zos/2.4.0?topic=SSLTBW_2.4.0/com.ibm.zos.v2r4.halc001/socket_parm.htm
-  **
-  ** (...) So the most correct thing to do is to use AF_INET in your struct sockaddr_in and PF_INET in your call to socket().
-  ** https://beej.us/guide/bgnet/html/#socket
-  */
+    memset(&server, 0, sizeof(server));
+    memset(&fd, 0, sizeof(fd));
 
-  if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-    // throw error  
-    std::cout << "error at socket()\n";
+    server.sin_family = AF_INET;
+    server.sin_port = htons(*it);
+    server.sin_addr.s_addr = INADDR_ANY;
+    // _servers.push_back(server);
+
+    if ((fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+      std::cout << "error socket()\n";
+    }
+
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+      close(fd);
+      std::cout << "error setsockopt()\n";
+      continue;
+    }
+
+    fcntl(fd, F_SETFL, O_NONBLOCK); // make socket non-blocking
+    if (bind(fd, reinterpret_cast<sockaddr *>(&server), addrlen) < 0) {
+      close(fd);
+      std::cout << "error bind()\n";
+      continue;
+    }
+
+    if (listen(fd, MAX_CONN) < 0) {
+      std::cout << "error listen()\n";
+      continue;
+    }
+
+    pfd.fd = fd;
+    pfd.events = POLLIN;
+
+    _pfds.push_back(pfd);
+    _listening.insert(std::pair<int, uint16_t>(pfd.fd, server.sin_port));
   }
+}
 
-  if (bind(sd, reinterpret_cast<struct sockaddr*>(&server), sizeof(server)) < 0) {
-    // throw error
-    std::cout << "error at bind()\n";
+void uServer::run(void) {
+  int events = 0;
+  int new_fd = 0;
+
+  struct sockaddr_in clientaddr;
+  socklen_t addrlen = sizeof(clientaddr);
+  struct pollfd client;
+
+  char buffer[10];
+  char msg[] = "hello world\n";
+  int nbytes = 0;
+
+  for (;;) {
+    if ((events = poll(_pfds.data(), _pfds.size(), -1)) < 0) {
+      std::cout << "error poll()\n";
+    }
+
+    for (size_t i = 0; events && i < _pfds.size(); ++i) {
+      if (_pfds[i].revents & POLLIN) {
+        /* if socket is one of the listening sockets make new connection */
+        if (_listening.find(_pfds[i].fd) != _listening.end()) {
+          memset(&clientaddr, 0, sizeof(clientaddr));
+
+          if (((new_fd =
+                    accept(_pfds[i].fd,
+                           reinterpret_cast<struct sockaddr *>(&clientaddr),
+                           &addrlen)) < 0)) {
+            std::cout << "error accept()\n";
+          }
+          memset(&client, 0, sizeof(client));
+          client.fd = new_fd;
+          client.events = POLLIN;
+          _pfds.push_back(client);
+          /* read or write */
+        } else {
+          memset(&buffer, 0, sizeof(buffer));
+          nbytes = recv(_pfds[i].fd, &buffer, sizeof(buffer), 0);
+
+          int sender = _pfds[i].fd;
+          if (nbytes <= 0) {
+            if (nbytes == 0) {
+              std::cout << "connection closed\n";
+            } else {
+              std::cout << "error recv()\n";
+            }
+            close(_pfds[i].fd);
+            _pfds.erase(_pfds.begin() + i);
+          } else {
+            std::cout << buffer;
+            send(sender, &msg, sizeof(msg), 0);
+          }
+        }
+      }
+    }
   }
+}
 
-  if (listen(sd, MAX_CONN) != 0) {
-    // throw error
-    std::cout << "error at listen()\n";
+void uServer::check() {
+  // std::cout << "the ports:" << std::endl;
+  // for (std::vector<sockaddr_in>::iterator it = _servers.begin();
+  //      it != _servers.end(); ++it) {
+  //   std::cout << ntohs(it->sin_port) << std::endl;
+  // }
+  // std::cout << std::endl;
+
+  std::cout << "# | POLLIN / POLLOUT" << std::endl;
+  for (std::vector<pollfd>::iterator it = _pfds.begin(); it != _pfds.end();
+       ++it) {
+    std::cout << it->fd;
+    if (it->events & POLLIN) {
+      std::cout << " | Alert me when data is ready to recv() on this socket.";
+    } else {
+      std::cout << " | Alert me when I can send() data to this socket without "
+                   "blocking.";
+    }
+    std::cout << std::endl;
   }
-  
-  memset(&client, 0, sizeof(client));
-  socklen_t namelen = sizeof(client);
-  if ((new_sd = accept(sd, reinterpret_cast<struct sockaddr*>(&client), &namelen)) < 0) {
-    // throw error
-    std::cout << "error at accept()\n";
-  }
+}
 
-  std::cout << "connection accepted\n";
+int main(void) {
+  /* testing ports */
+  std::vector<uint16_t> ports;
+  ports.push_back(static_cast<uint16_t>(8080));
+  ports.push_back(static_cast<uint16_t>(8081));
 
-  if (recv(new_sd, &buffer, sizeof(buffer), 0) < 0) {
-    // throw error
-    std::cout << "error at recv()\n";
-  }
+  /* initialize the server */
+  uServer serv(ports);
 
-  if (send(new_sd, &buffer, sizeof(buffer), 0) < 0) {
-    // throw error
-    std::cout << "error at send()\n";
-  }
+  /* create sockets for port listening */
+  serv.init();
 
-  close(sd);
-  close(new_sd);
+  /* testing purposes */
+  // serv.check();
+
+  serv.run();
 
   return 0;
 }
--- code/source_files/microserver.hpp
+++ code/source_files/microserver.hpp
@@ -4,26 +4,32 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <iostream>
+#include <map>
 #include <netdb.h>
 #include <poll.h>
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <utility>
 #include <vector>
 
 class uServer {
 public:
+  /* socket listening storage */
+  std::map<int, uint16_t> _listening;
+
+  /* initial ports to monitor */
   std::vector<uint16_t> _ports;
-  std::vector<sockaddr_in> _servers;
-  std::vector<pollfd> _sockets; // _sockets.data() will return a C-Style array
-                                // which is needed for poll()
+
+  /* active sockets to poll */
+  std::vector<pollfd> _pfds;
 
   uServer(void);
   uServer(std::vector<uint16_t> &ports);
   ~uServer(void);
 
-  void setup(void);
+  void init(void);
   void run(void);
   void check(void);
 };
