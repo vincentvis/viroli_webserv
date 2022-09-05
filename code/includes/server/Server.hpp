dcff86f2d289d0ecaf5dbd36fa1c8ed3520b1f3f
lisa_codam <lisa.vlamings@gmail.com>
2022-09-05 14:55:08 +0200

    copy constructor and assignment operator removed && e_methods and pragma once added

--- code/includes/server/Server.hpp
+++ code/includes/server/Server.hpp
@@ -1,37 +1,29 @@
-//
-// Created by Lisa Vlamings on 8/29/22.
-//
-
-#ifndef WEBSERV_SERVER_HPP
-#define WEBSERV_SERVER_HPP
+#pragma once
 
 #include <iostream>
 #include <stdint.h>
 #include <vector>
 
+enum e_methods { METHOD_UNDEFINED = 0, METHOD_GET = 1, METHOD_POST = 1 << 1, METHOD_DELETE = 1 << 2 };
+
 class Server
 {
 	protected:
-		uint16_t    _port;
-		std::string _serverName;
-		std::string _serverRoot;
-		int         _acceptedMethods; // 1,2,4
-		std::string _defaultIndexPage;
-		std::string _defaultErrorPage;
+		uint16_t       _port;
+		std::string    _serverName;
+		std::string    _serverRoot;
+		enum e_methods _acceptedMethods;
+		std::string    _defaultIndexPage;
+		std::string    _defaultErrorPage;
 		//		std::vector<ErrorPage>	_errorPages; // moet nog aangemaakt worden
-		std::string _routeType;
-		bool        _allowUpload;
-		bool        _autoIndex;
-		int         _bufferSize;
+		std::string    _routeType;
+		bool           _allowUpload;
+		bool           _autoIndex;
+		int            _bufferSize;
 		//		std::vector<Locations>	_locations; // moet nog aangemaakt worden
 
 	public:
 		//		Server(const ServerConfig &config); moet aangepast worden als parser af is
 		Server();
 		~Server();
-		Server(const Server &copy);
-		Server &operator=(const Server &copy);
 };
-
-
-#endif // WEBSERV_SERVER_HPP
--- code/source_files/server/Server.cpp
+++ code/source_files/server/Server.cpp
@@ -1,51 +1,20 @@
-//
-// Created by Lisa Vlamings on 8/29/22.
-//
-
 #include "server/Server.hpp"
 
 Server::Server()
 {
-	std::cout << "server default constructed is built" << std::endl;
+	std::cout << "server default constructer is built" << std::endl;
 	this->_port             = 0;
 	this->_serverName       = "empty";
 	this->_serverRoot       = "empty";
-	this->_acceptedMethods  = 0; // 1,2,4
+	this->_acceptedMethods  = METHOD_UNDEFINED;
 	this->_defaultIndexPage = "empty";
 	this->_defaultErrorPage = "empty";
 	//	this->_errorPages;
-	this->_routeType   = "empty";
-	this->_allowUpload = 0;
-	this->_autoIndex   = 0;
-	this->_bufferSize  = 0;
+	this->_routeType        = "empty";
+	this->_allowUpload      = 0;
+	this->_autoIndex        = 0;
+	this->_bufferSize       = 0;
 	//	this->_locations;
-}
-
-Server::Server(const Server &copy)
-    : _port(copy._port), _serverName(copy._serverName), _serverRoot(copy._serverRoot),
-      _acceptedMethods(copy._acceptedMethods), _defaultIndexPage(copy._defaultIndexPage), _routeType(copy._routeType),
-      _allowUpload(copy._allowUpload), _autoIndex(copy._autoIndex), _bufferSize(copy._bufferSize)
-{
-	//	this->_errorPages; // moet nog aangemaakt worden
-	//	this->_locations; // moet nog aangemaakt worden
-}
-
-
-Server &Server::operator=(const Server &copy)
-{
-	this->_port             = copy._port;
-	this->_serverName       = copy._serverName;
-	this->_serverRoot       = copy._serverRoot;
-	this->_acceptedMethods  = copy._acceptedMethods; // 1,2,4
-	this->_defaultIndexPage = copy._defaultIndexPage;
-	this->_defaultErrorPage = copy._defaultErrorPage;
-	//		std::vector<ErrorPage>    _errorPages  = copy._errorPages; // moet nog aangemaakt worden
-	this->_routeType   = copy._routeType;
-	this->_allowUpload = copy._allowUpload;
-	this->_autoIndex   = copy._autoIndex;
-	this->_bufferSize  = copy._bufferSize;
-	//		std::vector<Locations>   _locations = copy._locations; // moet nog aangemaakt worden
-	return (*this);
 }
 
 Server::~Server()
