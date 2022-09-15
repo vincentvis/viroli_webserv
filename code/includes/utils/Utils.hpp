d98259675f43b8a45a505fcd80c45dd9406ec137
Vincent <1089484+vincentvis@users.noreply.github.com>
2022-09-15 16:53:54 +0200

    Vi/utils (#14)
    * Utils in a namespace
    
    * I'm sorry, but the other thing was not working for me..
    
    * std exception for linux?
    
    * must it be stdstrings?
    
    * why is linux different from macos
    
    * Not a class
    maybe figure out at some point how namespaces work..
    
    Co-authored-by: Vincent <Codam>

--- code/includes/utils/Utils.hpp
+++ code/includes/utils/Utils.hpp
@@ -1,74 +1,16 @@
 #pragma once
 
 #include <cerrno>
-#include <cstdint>
 #include <exception>
+#include <stdexcept>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string>
 
-namespace Utils {
-
-	int64_t stol(const std::string &string) {
-		char *endptr = NULL;
-		errno        = 0;
-		long longval = strtol(string.c_str(), &endptr, 10);
-
-		if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
-			throw std::runtime_error(
-				"The string \"" + string + "\" does not contain a number"
-			);
-		}
-		return (longval);
-	}
-
-	std::string trimLeadingWhitespaceCopy(std::string str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (start != end && std::isspace(*start)) {
-			start++;
-		}
-		return (std::string(start, end));
-	}
-
-	std::string trimTrailingWhitespaceCopy(std::string str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (end - start > 0 && std::isspace(*end)) {
-			end--;
-		}
-		return (std::string(start, end));
-	}
-
-	std::string trimWhitespaceCopy(std::string str) {
-		return (trimTrailingWhitespaceCopy(trimLeadingWhitespaceCopy(str)));
-	}
-
-	void trimLeadingWhitespaceRef(std::string &str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (start != end && std::isspace(*start)) {
-			start++;
-		}
-		str = std::string(start, end) return (str);
-	}
-
-	void trimTrailingWhitespaceRef(std::string &str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (end - start > 0 && std::isspace(*end)) {
-			end--;
-		}
-		str = std::string(start, end);
-		return (str);
-	}
-
-	void trimWhitespaceRef(std::string &str) {
-		str = trimTrailingWhitespaceRef(trimLeadingWhitespaceRef(str));
-		return (str);
-	}
-
-} // namespace Utils
+int64_t     stol(const std::string &string);
+std::string trimLeadingWhitespaceCopy(std::string str);
+std::string trimTrailingWhitespaceCopy(std::string str);
+std::string trimWhitespaceCopy(std::string str);
+void        trimTrailingWhitespaceRef(std::string &str);
+void        trimLeadingWhitespaceRef(std::string &str);
+void        trimWhitespaceRef(std::string &str);
