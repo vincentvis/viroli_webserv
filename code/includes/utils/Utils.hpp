a7cbebc58675015b2bdbc4b6e9592becaed5a021
Vincent <Codam>
2022-09-15 16:33:31 +0200

    I'm sorry, but the other thing was not working for me..

--- code/includes/utils/Utils.hpp
+++ code/includes/utils/Utils.hpp
@@ -6,69 +6,13 @@
 #include <stdlib.h>
 #include <string>
 
-namespace Utils {
-
-	int64_t stol(const std::string &string) {
-		char *endptr = NULL;
-		errno        = 0;
-		long longval = strtol(string.c_str(), &endptr, 10);
-
-		if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
-			throw std::runtime_error(
-				"The string \"" + string + "\" does not contain a number"
-			);
-		}
-		return (longval);
-	}
-
-	std::string trimLeadingWhitespaceCopy(std::string str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (start != end && std::isspace(*start)) {
-			start++;
-		}
-		return (std::string(start, end));
-	}
-
-	std::string trimTrailingWhitespaceCopy(std::string str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (end - start > 0 && std::isspace(*end)) {
-			end--;
-		}
-		return (std::string(start, end));
-	}
-
-	std::string trimWhitespaceCopy(std::string str) {
-		return (trimTrailingWhitespaceCopy(trimLeadingWhitespaceCopy(str)));
-	}
-
-	void trimLeadingWhitespaceRef(std::string &str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (start != end && std::isspace(*start)) {
-			start++;
-		}
-		str = std::string(start, end) return (str);
-	}
-
-	void trimTrailingWhitespaceRef(std::string &str) {
-		std::string::iterator start = str.begin();
-		std::string::iterator end   = str.end();
-
-		while (end - start > 0 && std::isspace(*end)) {
-			end--;
-		}
-		str = std::string(start, end);
-		return (str);
-	}
-
-	void trimWhitespaceRef(std::string &str) {
-		str = trimTrailingWhitespaceRef(trimLeadingWhitespaceRef(str));
-		return (str);
-	}
-
-} // namespace Utils
+class Utils {
+	public:
+		static int64_t     stol(const std::string &string);
+		static std::string trimLeadingWhitespaceCopy(std::string str);
+		static std::string trimTrailingWhitespaceCopy(std::string str);
+		static std::string trimWhitespaceCopy(std::string str);
+		static void        trimTrailingWhitespaceRef(std::string &str);
+		static void        trimLeadingWhitespaceRef(std::string &str);
+		static void        trimWhitespaceRef(std::string &str);
+};
--- code/source_files/utils/Utils.cpp
+++ code/source_files/utils/Utils.cpp
@@ -1,1 +1,64 @@
+#include "utils/Utils.hpp"
 // utils?
+
+int64_t Utils::stol(const std::string &string) {
+	char *endptr = NULL;
+	errno        = 0;
+	long longval = strtol(string.c_str(), &endptr, 10);
+
+	if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
+		throw std::runtime_error(
+			"The string \"" + string + "\" does not contain a number"
+		);
+	}
+	return (longval);
+}
+
+std::string Utils::trimLeadingWhitespaceCopy(std::string str) {
+	std::string::iterator start = str.begin();
+	std::string::iterator end   = str.end();
+
+	while (start != end && std::isspace(*start)) {
+		start++;
+	}
+	return (std::string(start, end));
+}
+
+std::string Utils::trimTrailingWhitespaceCopy(std::string str) {
+	std::string::iterator start = str.begin();
+	std::string::iterator end   = str.end();
+
+	while (end - start > 0 && std::isspace(*end)) {
+		end--;
+	}
+	return (std::string(start, end));
+}
+
+std::string Utils::trimWhitespaceCopy(std::string str) {
+	return (Utils::trimTrailingWhitespaceCopy(Utils::trimLeadingWhitespaceCopy(str)));
+}
+
+void Utils::trimLeadingWhitespaceRef(std::string &str) {
+	std::string::iterator start = str.begin();
+	std::string::iterator end   = str.end();
+
+	while (start != end && std::isspace(*start)) {
+		start++;
+	}
+	str = std::string(start, end);
+}
+
+void Utils::trimTrailingWhitespaceRef(std::string &str) {
+	std::string::iterator start = str.begin();
+	std::string::iterator end   = str.end();
+
+	while (end - start > 0 && std::isspace(*end)) {
+		end--;
+	}
+	str = std::string(start, end);
+}
+
+void Utils::trimWhitespaceRef(std::string &str) {
+	Utils::trimLeadingWhitespaceRef(str);
+	Utils::trimTrailingWhitespaceRef(str);
+}
