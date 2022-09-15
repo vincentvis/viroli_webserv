ecec60baa2bb8983a7a802e9577dad7e4847f97a
Vincent <1089484+vincentvis@users.noreply.github.com>
2022-09-15 16:12:18 +0200

    Utils in a namespace (#13)
    Co-authored-by: Vincent <Codam>
    Approved by: Lisa

--- code/source_files/main.cpp
+++ code/source_files/main.cpp
@@ -2,12 +2,10 @@
 #include "server/Server.hpp"
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	Server Default;
 
 	std::cout << "Ran " << argv[0] << " with argc: " << argc << std::endl;
 
 	return 0;
 }
-
