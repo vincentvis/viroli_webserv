f843c903a8ff8ae2a5eb27867cedb7d6698ef1ad
Vincent <1089484+vincentvis@users.noreply.github.com>
2022-09-16 13:16:24 +0200

    Vi/utils in namespace (#15)
    * Utils in namespace
    
    * make stol overload itself if you pass a non-const and bool
    
    Co-authored-by: Vincent <Codam>

--- code/includes/utils/Utils.hpp
+++ code/includes/utils/Utils.hpp
@@ -7,10 +7,15 @@
 #include <stdlib.h>
 #include <string>
 
-int64_t     stol(const std::string &string);
-std::string trimLeadingWhitespaceCopy(std::string str);
-std::string trimTrailingWhitespaceCopy(std::string str);
-std::string trimWhitespaceCopy(std::string str);
-void        trimTrailingWhitespaceRef(std::string &str);
-void        trimLeadingWhitespaceRef(std::string &str);
-void        trimWhitespaceRef(std::string &str);
+namespace Utils {
+
+	int64_t     stol(std::string &string, bool skip);
+	int64_t     stol(const std::string &string);
+	std::string trimLeadingWhitespaceCopy(std::string str);
+	std::string trimTrailingWhitespaceCopy(std::string str);
+	std::string trimWhitespaceCopy(std::string str);
+	void        trimTrailingWhitespaceRef(std::string &str);
+	void        trimLeadingWhitespaceRef(std::string &str);
+	void        trimWhitespaceRef(std::string &str);
+
+} // namespace Utils
--- code/source_files/utils/Utils.cpp
+++ code/source_files/utils/Utils.cpp
@@ -1,64 +1,81 @@
 #include "utils/Utils.hpp"
 
-int64_t stol(const std::string &string) {
-	char *endptr = NULL;
-	errno        = 0;
-	long longval = strtol(string.c_str(), &endptr, 10);
+namespace Utils {
+	int64_t stol(std::string &string, bool skip) {
+		char *endptr    = NULL;
+		errno           = 0;
+		int64_t longval = strtol(string.c_str(), &endptr, 10);
 
-	if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
-		throw std::runtime_error(
-			std::string("The string \"") + string +
-			std::string("\" does not contain a number")
-		);
+		if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
+			throw std::runtime_error(
+				"The string \"" + string + "\" does not contain a number"
+			);
+		}
+		if (skip) {
+			string = string.substr(endptr - string.c_str());
+		}
+		return (longval);
 	}
-	return (longval);
-}
 
-std::string trimLeadingWhitespaceCopy(std::string str) {
-	std::string::iterator start = str.begin();
-	std::string::iterator end   = str.end();
+	int64_t stol(const std::string &string) {
+		char *endptr    = NULL;
+		errno           = 0;
+		int64_t longval = strtol(string.c_str(), &endptr, 10);
 
-	while (start != end && std::isspace(*start)) {
-		start++;
+		if (errno == EINVAL || errno == ERANGE || string.c_str() == endptr) {
+			throw std::runtime_error(
+				"The string \"" + string + "\" does not contain a number"
+			);
+		}
+		return (longval);
 	}
-	return (std::string(start, end));
-}
 
-std::string trimTrailingWhitespaceCopy(std::string str) {
-	std::string::iterator start = str.begin();
-	std::string::iterator end   = str.end();
+	std::string trimLeadingWhitespaceCopy(std::string str) {
+		std::string::iterator start = str.begin();
+		std::string::iterator end   = str.end();
 
-	while (end - start > 0 && std::isspace(*end)) {
-		end--;
+		while (start != end && std::isspace(*start)) {
+			start++;
+		}
+		return (std::string(start, end));
 	}
-	return (std::string(start, end));
-}
 
-std::string trimWhitespaceCopy(std::string str) {
-	return (trimTrailingWhitespaceCopy(trimLeadingWhitespaceCopy(str)));
-}
+	std::string trimTrailingWhitespaceCopy(std::string str) {
+		std::string::iterator start = str.begin();
+		std::string::iterator end   = str.end();
 
-void trimLeadingWhitespaceRef(std::string &str) {
-	std::string::iterator start = str.begin();
-	std::string::iterator end   = str.end();
-
-	while (start != end && std::isspace(*start)) {
-		start++;
+		while (end - start > 0 && std::isspace(*end)) {
+			end--;
+		}
+		return (std::string(start, end));
 	}
-	str = std::string(start, end);
-}
 
-void trimTrailingWhitespaceRef(std::string &str) {
-	std::string::iterator start = str.begin();
-	std::string::iterator end   = str.end();
-
-	while (end - start > 0 && std::isspace(*end)) {
-		end--;
+	std::string trimWhitespaceCopy(std::string str) {
+		return (trimTrailingWhitespaceCopy(trimLeadingWhitespaceCopy(str)));
 	}
-	str = std::string(start, end);
-}
 
-void trimWhitespaceRef(std::string &str) {
-	trimLeadingWhitespaceRef(str);
-	trimTrailingWhitespaceRef(str);
-}
+	void trimLeadingWhitespaceRef(std::string &str) {
+		std::string::iterator start = str.begin();
+		std::string::iterator end   = str.end();
+
+		while (start != end && std::isspace(*start)) {
+			start++;
+		}
+		str = std::string(start, end);
+	}
+
+	void trimTrailingWhitespaceRef(std::string &str) {
+		std::string::iterator start = str.begin();
+		std::string::iterator end   = str.end();
+
+		while (end - start > 0 && std::isspace(*end)) {
+			end--;
+		}
+		str = std::string(start, end);
+	}
+
+	void trimWhitespaceRef(std::string &str) {
+		trimLeadingWhitespaceRef(str);
+		trimTrailingWhitespaceRef(str);
+	}
+} // namespace Utils
