7231b7a50bef9f4445538c7b13613e2c461b0439
Ronald Heuts <rheuts@f0r6s6.codam.nl>
2022-09-28 16:24:37 +0200

    useful python script to simulate requests

---
+++ code/source_files/client.py
@@ -0,0 +1,23 @@
+import socket
+import requests
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 8080  # The port used by the server
+
+def gen():
+  yield b'hello'
+  yield b'frankrijk'
+
+def sendRequest(host, port):
+  # requests.get(f'http://{host}:{port}')
+  requests.post(f'http://{host}:{port}', data=gen())
+  # print(r)
+
+  # print(r)
+  # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  #     s.connect((HOST, PORT))
+  #     s.sendall(b'hello world')
+  #     data = s.recv(1024)
+
+if  __name__ == '__main__':
+  sendRequest(HOST, 8080)
