26d88d35656b871d97efe358cb7a231f6331e3bd
lisa_codam <livlamin@student.codam.nl>
2022-10-05 15:12:10 +0200

    without resp

--- code/source_files/request/HttpRequest.cpp
+++ code/source_files/request/HttpRequest.cpp
@@ -20,19 +20,16 @@ void HttpRequest::CheckMethod(const Request &Req, const Config &Conf, Response &
 void HttpRequest::GETRequest(const Request &Req, const Config &Conf, Response &Res) {
 	Res.initResponse("200", Conf, Req);
 	Res.createResponseHeader();
-//	Res.setRespReady();
 }
 
 void HttpRequest::POSTRequest(const Request &Req, const Config &Conf, Response &Res) {
 	Res.initResponse("200", Conf, Req);
 	Res.createResponseHeader();
-//	Res.setRespReady();
 }
 
 void HttpRequest::DELETERequest(const Request &Req, const Config &Conf, Response &Res) {
 	Res.initResponse("200", Conf, Req);
 	Res.createResponseHeader();
-//	Res.setRespReady();
 }
 
 HttpRequest::~HttpRequest() {
