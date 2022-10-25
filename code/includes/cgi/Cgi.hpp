0951b03cbd3862b461987f99c760a893cbc313da
Vincent <Codam>
2022-10-25 12:32:10 +0200

    kut examen

--- code/includes/cgi/Cgi.hpp
+++ code/includes/cgi/Cgi.hpp
@@ -3,30 +3,30 @@
 #include "cgi/CgiVars.hpp"
 #include "cgi/Pipes.hpp"
 #include "utils/FileStat.hpp"
+#include <string.h>
 #include <unistd.h>
 
 class Cgi {
 	public:
 		Cgi(const FileStat &filestats);
 		~Cgi();
-		void        setBody(std::string &body);
-		void        setQuery(std::string &body);
+		void         setBody(std::string &body);
+		void         setQuery(std::string &body);
 
-		bool        isDone() const;
-		std::string getStatusCode() const;
-
+		bool         isDone() const;
+		std::string  getStatusCode() const;
+		Cgi          setQueryString(std::string queryString);
+		char *const *makeArgv() const;
 
 	private:
 		Cgi();
-		void operator=(Cgi const &);
-		Cgi(Cgi const &);
-		FileStat const &_source;
-		std::string     _executor_name;
-		std::string     _executor_path;
-		std::string     _script_name;
-		std::string     _statusCode;
-		bool            _done;
-		Pipes           _pipes;
-		CgiVars         _env;
-		CgiVars         _args;
+		FileStat const          &_source;
+		std::string              _executor_name;
+		std::string              _executor_path;
+		std::string              _script_name;
+		std::string              _statusCode;
+		bool                     _done;
+		Pipes                    _pipes;
+		CgiVars                  _env;
+		std::vector<std::string> _args;
 };
--- code/source_files/cgi/Cgi.cpp
+++ code/source_files/cgi/Cgi.cpp
@@ -12,6 +12,7 @@ Cgi::Cgi(const FileStat &filestats) : _source(filestats) {
 	std::cout << "Script executor: " << this->_executor_name << std::endl;
 	std::cout << "Script name: " << this->_script_name << std::endl;
 	std::cout << "Script path: " << _source.getPath() << std::endl;
+	_args.push_back(_source.getFull);
 
 	try {
 		_pipes.openPipes();
@@ -25,6 +26,25 @@ Cgi::Cgi(const FileStat &filestats) : _source(filestats) {
 Cgi::~Cgi() {
 }
 
+Cgi Cgi::setQueryString(std::string queryString) {
+	_args.push_back(queryString);
+	return (*this);
+}
+
+char *const *Cgi::makeArgv() const {
+	std::vector<std::string>::iterator it   = _args.begin();
+	std::vector<std::string>::iterator end  = _args.end();
+	char                             **argv = new char *[_args.size() + 1];
+	int                                i    = 0;
+
+	while (it != end) {
+		argv[i] = new char[it->length + 1];
+		memcpy(argv[i], it->c_str(), it->length() + 1);
+		it++;
+		i++;
+	}
+	return (argv);
+}
 
 bool Cgi::isDone() const {
 	return (_done);
