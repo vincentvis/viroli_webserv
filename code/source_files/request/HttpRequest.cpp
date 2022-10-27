3b47ba2cd343e014991ce8b1272486ac22ed5562
lisa_codam <livlamin@student.codam.nl>
2022-10-27 17:23:30 +0200

    post questionmark

--- code/source_files/request/HttpRequest.cpp
+++ code/source_files/request/HttpRequest.cpp
@@ -19,7 +19,7 @@ HttpRequest::HttpRequest(ClientFD &Client) {
 
 void HttpRequest::GETRequest(ClientFD &Client) {
 	std::string path = Client._request.getFileStat().getFull();
-	int fd;
+	int         fd;
 
 	if (Client._request.getFileStat().isDir()) {
 		if (Client._config->getAutoIndex(Client._location) == "on") {
@@ -38,9 +38,6 @@ void HttpRequest::GETRequest(ClientFD &Client) {
 		std::vector<std::string>::const_iterator it  = indexes.begin();
 		std::vector<std::string>::const_iterator end = indexes.end();
 		std::string                              tmp;
-//		if (*path.rbegin() != '/') { // this can be deleted now right?
-//			path += "/";
-//		}
 		fd = -1;
 		while (it != end) {
 			tmp = path + *it;
@@ -56,32 +53,22 @@ void HttpRequest::GETRequest(ClientFD &Client) {
 	} else {
 		fd = open(path.c_str(), O_RDONLY);
 	}
-	if (fd == -1) { // is this going well since it will also catches the -1 of de index on?
-		throw Utils::ErrorPageException("404"); // if change back to errorResponse do RETURN!
+	if (fd == -1) {
+		throw Utils::ErrorPageException(
+			"404"); // if change back to errorResponse do RETURN!
 	}
 	/* add fileFd to poll */
 	Client._fileFD = reinterpret_cast<FileFD *>(
 		Server::addPollable(Client._server, fd, FILEPOLL, POLLIN));
 	Client._fileFD->setRequestInterface(this, &Client);
 }
-#include "config/MimeTypes.hpp"
 
 void HttpRequest::POSTRequest(ClientFD &Client) {
-	/* check if requested file is valid */
-
-
-	/* check if path to upload exists// is already done ? */
-
-	/* check if file exists */
-//	Client._request.getFileStat().getFilename();
-	
-//	std::string con = MimeTypes::findMimeType(Client, Client._request.getFileStat().getFilename());
-
-	int fd = open(Client._request.getFileStat().getFull().c_str(), O_TRUNC | O_CREAT | O_WRONLY, S_IRWXU); // change?
+	int fd = open(Client._request.getFileStat().getFull().c_str(),
+				  O_TRUNC | O_CREAT | O_WRONLY, S_IRWXU); // change?
 	if (fd == -1) {
 		throw Utils::ErrorPageException("404");
 	} else {
-		//		set location in response header ?is this still necessary?
 		Client._fileFD = reinterpret_cast<FileFD *>(
 			Server::addPollable(Client._server, fd, FILEPOLL, POLLOUT));
 		if (!Client.getBodyStr().empty()) {
@@ -92,16 +79,18 @@ void HttpRequest::POSTRequest(ClientFD &Client) {
 }
 
 // If a DELETE method is successfully applied, the origin server SHOULD send
-// a 202 (Accepted) status code if the action will likely succeed but has not yet been enacted,
-//	a 204 (No Content) status code if the action has been enacted and no further information is to be supplied, or
-//	a 200 (OK) status code if the action has been enacted and the response message includes a representation describing the status.
-
+// a 202 (Accepted) status code if the action will likely succeed but has not yet been
+// enacted,
+//	a 204 (No Content) status code if the action has been enacted and no further
+// information is to be supplied, or 	a 200 (OK) status code if the action has been
+// enacted and the response message includes a representation describing the status.
 void HttpRequest::DELETERequest(ClientFD &Client) {
-	if (remove(Client._request.getFileStat().getFull().c_str()) != 0){
-		throw Utils::ErrorPageException("404"); // would you agree with 404?
-	}
-	else {
+	if (Client._request.getFileStat().isReg() &&
+		remove(Client._request.getFileStat().getFull().c_str()) == 0)
+	{
 		Client._response.generateResponse(&Client, "200");
+	} else {
+		throw Utils::ErrorPageException("404"); // would you agree with 404?
 	}
 }
 
