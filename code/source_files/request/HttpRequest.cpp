cf017d7c75c78eafef55e68d8764e591fc8101d3
lisa_codam <livlamin@student.codam.nl>
2022-10-27 15:33:10 +0200

    .

--- code/source_files/request/HttpRequest.cpp
+++ code/source_files/request/HttpRequest.cpp
@@ -19,20 +19,25 @@ HttpRequest::HttpRequest(ClientFD &Client) {
 
 void HttpRequest::GETRequest(ClientFD &Client) {
 
-		std::cout << "fileName [" <<  Client._request.getFileStat().getFilename() << "]" << std::endl;
-		std::cout << "pathName [" <<  Client._request.getFileStat().getPath() << "]" << std::endl;
-		std::cout << "getExtention [" <<  Client._request.getFileStat().getExtension() << "]" << std::endl;
-		std::cout << "getFull [" <<  Client._request.getFileStat().getFull() << "]" << std::endl;
-//	std::string path = Client._config->getRoot(Client._location);
-//	std::string uri  = Client._request.getUri();
-//	if (*path.rbegin() != '/' && (uri.empty() == false && uri.at(0) != '/')) {
-//		path += "/";
-//	}
-//	path += uri;
+//	std::cout << "fileName [" <<  Client._request.getFileStat().getFilename() << "]" << std::endl;
+//	std::cout << "pathName [" <<  Client._request.getFileStat().getPath() << "]" << std::endl;
+//	std::cout << "getExtention [" <<  Client._request.getFileStat().getExtension() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().getFull() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().isDir() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().isExecutable() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().isReadable() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().isReg() << "]" << std::endl;
+//	std::cout << "getFull [" <<  Client._request.getFileStat().isWriteable() << "]" << std::endl;
+	//	std::string path = Client._config->getRoot(Client._location); // I think we can delete this right?
+	//	std::string uri  = Client._request.getUri();
+	//	if (*path.rbegin() != '/' && (uri.empty() == false && uri.at(0) != '/')) {
+	//		path += "/";
+	//	}
+	//	path += uri;
 	std::string path = Client._request.getFileStat().getFull();
 	int fd;
 
-	if (Client._request.uriIsDir()) {
+	if (Client._request.getFileStat().isDir()) {
 		if (Client._config->getAutoIndex(Client._location) == "on") {
 			try {
 				Autoindex autoindex(path);
@@ -42,19 +47,16 @@ void HttpRequest::GETRequest(ClientFD &Client) {
 			} catch (const Utils::AutoindexException &e) {
 				throw Utils::ErrorPageException("404");
 			} catch (const std::exception &e) {
-				// all other exceptions?
-				// but what to do?
-				// internal server error for now
-				throw Utils::ErrorPageException("500");
+				throw Utils::ErrorPageException("500"); // internal server error?
 			}
 		}
 		std::vector<std::string> indexes = Client._config->getIndex(Client._location);
 		std::vector<std::string>::const_iterator it  = indexes.begin();
 		std::vector<std::string>::const_iterator end = indexes.end();
 		std::string                              tmp;
-		if (*path.rbegin() != '/') {
-			path += "/";
-		}
+//		if (*path.rbegin() != '/') { // not sure if we need this?
+//			path += "/";
+//		}
 		fd = -1;
 		while (it != end) {
 			tmp = path + *it;
@@ -70,7 +72,7 @@ void HttpRequest::GETRequest(ClientFD &Client) {
 	} else {
 		fd = open(path.c_str(), O_RDONLY);
 	}
-	if (fd == -1) {
+	if (fd == -1) { // is this going well since it will also catches the -1 of de index on?
 		throw Utils::ErrorPageException("404"); // if change back to errorResponse do RETURN!
 	}
 	/* add fileFd to poll */
@@ -99,7 +101,7 @@ void HttpRequest::POSTRequest(ClientFD &Client) {
 	}
 }
 
-//If a DELETE method is successfully applied, the origin server SHOULD send
+// If a DELETE method is successfully applied, the origin server SHOULD send
 // a 202 (Accepted) status code if the action will likely succeed but has not yet been enacted,
 //	a 204 (No Content) status code if the action has been enacted and no further information is to be supplied, or
 //	a 200 (OK) status code if the action has been enacted and the response message includes a representation describing the status.
